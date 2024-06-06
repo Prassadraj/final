c8d5e7df73f08674f6f9f52b7da8256f62870c69
Prassadraj <prassadpr10@gmail.com>
2024-06-06 11:03:39 +0530

    chg

--- src/homepages/Section3/Section3.jsx
+++ src/homepages/Section3/Section3.jsx
@@ -1,7 +1,22 @@
 import React from "react";
-
+import image1 from "../../images/section2&3/image1.png";
+import image2 from "../../images/section2&3/image2.png";
 function Section3() {
-  return <div></div>;
+  return (
+    <div>
+      <div className="section3">
+        <div className="left"></div>
+        <div className="right">
+          <div className="relative">
+            <img src={image1} alt="" />
+            <div className="absolute">
+              <img src={image2} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Section3;
