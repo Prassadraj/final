ec89137abc63dc990bd8996836c01f7aa8224913
Prassadraj <prassadpr10@gmail.com>
2024-07-30 15:04:42 +0530

    chg

--- src/homepages/FeaturedProducts/FeaturedProducts.jsx
+++ src/homepages/FeaturedProducts/FeaturedProducts.jsx
@@ -49,7 +49,7 @@ const FeaturedProducts = () => {
   }, []);
 
   return (
-    <div className="pt-10 hidden md:block">
+    <div className="">
       <div className="flex h-10 items-center md:pl-24 flex-col md:gap-4 heading p-2 text-center">
         <span className="font-semibold text-xl md:text-4xl uppercase heading font-poppins text-custom-green">
           Featured Products
@@ -59,7 +59,9 @@ const FeaturedProducts = () => {
           provide the best productsto our patients.
         </p>
       </div>
-      <HorizontalScrollCarousel cards={featuredCards} />
+      <div className="">
+        <HorizontalScrollCarousel cards={featuredCards} />
+      </div>
     </div>
   );
 };
@@ -70,10 +72,14 @@ const HorizontalScrollCarousel = ({ cards }) => {
     target: targetRef,
   });
 
+  // Adjust these values based on your needs for mobile responsiveness
   const x = useTransform(scrollYProgress, [0, 1.6], ["1%", "-95%"]);
 
   return (
-    <section ref={targetRef} className="relative h-[300vh] products">
+    <section
+      ref={targetRef}
+      className="relative h-[70vh] md:h-[300vh] products"
+    >
       <div className="sticky top-0 flex md:h-screen items-center overflow-hidden">
         <motion.div style={{ x }} className="flex gap-4 md:gap-8">
           {cards.slice(0, 7).map((card) => (
@@ -88,22 +94,22 @@ const HorizontalScrollCarousel = ({ cards }) => {
 
 const Card = ({ card }) => {
   return (
-    <div className="font-poppins` relative h-[300px] w-[200px] md:h-[400px] md:w-[350px] overflow-hidden bg-light-green rounded-2xl">
-      <div className="flex justify-center items-center w-full md:mt-10 h-44">
+    <div className="mt-2 font-poppins` relative h-[250px] w-[150px] md:h-[400px] md:w-[350px] overflow-hidden bg-light-green rounded-2xl">
+      <div className="flex justify-center items-center w-full md:mt-10 h-32">
         {/* <Tilt scale={1.2} transitionSpeed={2500}> */}
-        <img width="150px" src={card.url} alt="" />
+        <img className="md:w-[150px] w-[100px]" src={card.url} alt="" />
         {/* </Tilt> */}
       </div>
       <div className="absolute md:top-60 md:left-10 md:right-10 p-0">
         <div className="flex justify-center md:justify-start">
           <p className="md:text-xl font-poppins font-bold ">{card.title}</p>
         </div>
-        <p className="text-sm md:text-base line-clamp-3 px-1">{card.content}</p>
+        <p className="text-xs md:text-base line-clamp-3 px-1">{card.content}</p>
       </div>
-      <div className="absolute bottom-1 md:bottom-6 w-full">
+      <div className="absolute bottom-3 md:bottom-6 w-full">
         <div className="flex justify-center items-center">
           <Magnetic>
-            <button className="w-fit md:w-72 bg-custom-green p-1 md:px-3 md:py-2 rounded-lg text-white">
+            <button className="text-xs w-fit md:w-72 bg-custom-green p-0.5 md:px-3 md:py-2 rounded-lg text-white">
               View Product
             </button>
           </Magnetic>
@@ -115,16 +121,16 @@ const Card = ({ card }) => {
 
 const AllProductsCard = () => {
   return (
-    <div className="group relative h-[400px] w-[200px] overflow-hidden flex items-center justify-center">
+    <div className="group relative h-[400px] w-[100px]  md:w-[200px] overflow-hidden flex items-center justify-center">
       <div className="absolute inset-0 z-0"></div>
       <div className="absolute inset-0 z-10 flex items-center justify-center">
         <Magnetic>
-          <div
-            className="text-xl font-black text-custom-green p-2 rounded-xl cursor-pointer"
-            style={{ fontFamily: "Poppins" }}
-          >
+          <div className="mr-10 text-xs md:text-xl font-black text-custom-green p-2 rounded-xl cursor-pointer">
             <Link to="/product">
-              All Products <FontAwesomeIcon icon={faArrowRight} />
+              <span className="hidden md:block">All Products</span>
+              <span>
+                <FontAwesomeIcon icon={faArrowRight} />
+              </span>
             </Link>
           </div>
         </Magnetic>
